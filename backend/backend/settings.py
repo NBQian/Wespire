a594a8401e7b09be304d194e6ff2368e1aee36ea
Niu BoQian <e0774823@u.nus.edu>
2024-02-22 16:43:21 -0800

    completed serving all static files from S3. Removed redundant files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,8 +6,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')

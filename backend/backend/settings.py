38bf574a9152965a6cfd5254bc774e0907bf38bf
Niu BoQian <e0774823@u.nus.edu>
2024-02-22 22:50:53 -0800

    modified .env file in frontend to use the public IPv4 url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-p-ia7h6cdm@1cp#0yaidr#fgha_3$c#7#iaazo9up_t60i$hh$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 

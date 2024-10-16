b75c3bf26ecb635884461d53506292c82dba3526
Niu BoQian <105575052+NBQian@users.noreply.github.com>
2024-10-16 23:22:06 +0800

    removed unnecessary comments

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -213,17 +213,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'wespireutilities@gmail.com'
 EMAIL_HOST_PASSWORD = "rono gdwc fzlh zeee" 
 EMAIL_USE_TLS = True
-
-
-# AWS_ACCESS_KEY_ID = 'AKIAZI2LIVEZ64VSR2V2'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'wespirebackend'
-# AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = 'ap-southeast-1'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  None
-# AWS_S3_VERIFY = True
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
